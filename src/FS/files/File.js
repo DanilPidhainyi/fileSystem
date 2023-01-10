07ca4f957111202e611257325b16ac9e0edaa60d
Danil Pidhainyi <danylo.pidgainyi@gmail.com>
2023-01-11 01:42:50 +0200

    - file

--- src/FS/files/File.js
+++
@@ -1,8 +0,0 @@
-import {Descriptor} from "../blocks/Descriptor.mjs";
-import {DIRECTORY} from "../static/constants.mjs";
-
-// export const file = {
-//     createDirectory(pathname) {
-//         return new Descriptor(DIRECTORY, 0, 0, [])
-//     }
-// }
